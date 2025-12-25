cc1d3362568bfe8002058c475ae9aaa62d13a3cc
Sardaru Andreea-Miruna <mirunasardaru15@gmail.com>
2025-12-25 15:27:46 +0200

    Add files via upload

---
+++ POO_PROIECT/config.h
@@ -0,0 +1,20 @@
+//
+// Created by HP-15S on 12/25/2025.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <fstream>
+#include <sstream>
+
+class ConfigFile {
+    static Config* obj;
+
+    ConfigFile() {
+
+    }
+    ~ConfigFile() = default;
+};
+
+#endif //CONFIG_H
